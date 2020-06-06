265ce44b7a534cc41ecb685d9db067db070a9b6b
Analia Mok <amok@geekhive.com>
2020-06-06 13:04:14 -0400

    Styled form container and basic buttons

---
+++ web/themes/custom/tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif',],
+      },
+    },
+  },
+  variants: {},
+  plugins: []
+}
